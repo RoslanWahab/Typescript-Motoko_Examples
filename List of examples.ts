cf966bce25e1aef12791fcb9b7f7e3aca96474eb
RoslanWahab <134185053+RoslanWahab@users.noreply.github.com>
2023-10-28 17:53:19 +0800

    Update List of examples.ts

--- List of examples.ts
+++ List of examples.ts
@@ -11,11 +11,14 @@ console.log(a); //print a
 console.log(b); //print b
 console.log(c); //print c
 
+//----------------------------------------------------------
 //Ex 2 - more checking exercise
 function squareOf(n) {
 return n * n
 }
 squareOf(2) // evaluates to 4
 squareOf('z') // evaluates to NaN
+
 console.log(squareOf(2)); //print a
-console.log(b); //print b
+console.log(squareOf('z')); //print b
+//----------------------------------------------------------
